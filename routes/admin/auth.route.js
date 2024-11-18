5aae24268772025f698189bc2e23e0f2e53a9b3d
KProject <158186819+khaicoderproject@users.noreply.github.com>
2024-11-18 13:05:23 +0700

    vercel

--- routes/admin/auth.route.js
+++ routes/admin/auth.route.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const controllerAdmin = require("../../controllers/admin/auth.controller");
 router.get("/login", controllerAdmin.login);
 router.post("/login", controllerAdmin.loginPost);
-router.get("register-admin", controllerAdmin.register);
-router.post("register-admin", controllerAdmin.registerPost);
+router.get("/register-admin", controllerAdmin.register);
+router.post("/register-admin", controllerAdmin.registerPost);
 module.exports = router;
